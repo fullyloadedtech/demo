05dc31c9f0f2791952123183c46b3ccc8a8c25b7
FullyLoadedTech <fullyloadedtech@protonmail.com>
2022-09-19 22:03:37 +0000

    fix calendar widget

--- app/Filament/Resources/EventResource/Widgets/CalendarWidget.php
+++ app/Filament/Resources/EventResource/Widgets/CalendarWidget.php
@@ -2,9 +2,30 @@
 
 namespace App\Filament\Resources\EventResource\Widgets;
 
-use Filament\Widgets\Widget;
+//use Filament\Widgets\Widget;
+use Saade\FilamentFullCalendar\Widgets\FullCalendarWidget;
 
-class CalendarWidget extends Widget
+class CalendarWidget extends FullCalendarWidget
 {
-    protected static string $view = 'filament.resources.event-resource.widgets.calendar-widget';
+//    protected static string $view = 'filament.resources.event-resource.widgets.calendar-widget';
+    /**
+     * Return events that should be rendered statically on calendar.
+     */
+    public function getViewData(): array
+    {
+        return [
+            [
+                'id' => 1,
+                'title' => 'Breakfast!',
+                'start' => now()
+            ],
+            [
+                'id' => 2,
+                'title' => 'Meeting with Pamela',
+                'start' => now()->addDay(),
+                'url' => 'https://some-url.com',
+                'shouldOpenInNewTab' => true,
+            ]
+        ];
+    }
 }
